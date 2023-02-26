e7b5fc4a788f649322b9004743f01be66fa445d1
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-26 12:42:58 -0300

    Add packages

--- src/edu/mrodrigochaves/average/SchoolReport.java
+++ src/edu/mrodrigochaves/average/SchoolReport.java
@@ -1,4 +1,5 @@
-package average;
+package edu.mrodrigochaves.average;
+
 public class SchoolReport {
 
     public static void main(String[] args) {
--- src/edu/mrodrigochaves/hello/Hello.java
+++ src/edu/mrodrigochaves/hello/Hello.java
@@ -1,4 +1,5 @@
-public class App {
+package edu.mrodrigochaves.hello;
+public class Hello {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
