254f8c7906edb11ba12085e8262f138741755eaf
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-26 11:43:56 -0300

    add Class

---
+++ hello/src/SchoolReport.java
@@ -0,0 +1,14 @@
+public class SchoolReport {
+
+    public static void main(String[] args) {
+        int finalAverage = 7;
+        if (finalAverage < 6)
+            System.out.println("You have been disapproved!");
+        else if (finalAverage == 6)
+            System.out.println("You need retake test!");
+        else
+            System.out.println("Approved!");
+        
+    }
+    
+}
