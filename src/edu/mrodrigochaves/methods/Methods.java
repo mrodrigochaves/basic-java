a09c7a05c183a68040ba9bf1283b184de5c99c52
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-27 19:50:35 -0300

    Add Class methods

---
+++ src/edu/mrodrigochaves/methods/Methods.java
@@ -0,0 +1,13 @@
+package edu.mrodrigochaves.methods;
+//somar (int n1, int n2){}
+//abrirConexao(){}
+//concluirProcessamento(){}
+
+public class Methods {
+    public void somar() {
+        
+    }
+    public void imprimir() {
+        
+    }
+}
