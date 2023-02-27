e10e7967a5a12305ee881c415585a782f4ebcacc
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-27 18:55:04 -0300

    add operators

---
+++ src/edu/mrodrigochaves/operators/Operators.java
@@ -0,0 +1,17 @@
+package edu.mrodrigochaves.operators;
+
+public class Operators {
+        public static void main(String[] args) {
+          int numero1 = 3;
+          int numero2 = 2;
+            
+          if(numero1 < numero2){
+          System.out.println("Sim é verdade!");
+        }
+          else if(numero1 > numero2){
+            System.out.println("Não, ele é menor.");
+        }
+          
+    }
+}
+
