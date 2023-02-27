e982766eb51d3e46f7595fc99c0f5ecf4a4ebcd1
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-27 18:59:09 -0300

    Add equals method

--- src/edu/mrodrigochaves/operators/Operators.java
+++ src/edu/mrodrigochaves/operators/Operators.java
@@ -2,7 +2,15 @@ package edu.mrodrigochaves.operators;
 
 public class Operators {
         public static void main(String[] args) {
-          int numero1 = 3;
+         String nomeUm = "Márcio";
+         String nomeDois = new String("Rodrigo");
+
+
+         System.out.println(nomeUm.equals(nomeDois));
+         
+         
+         
+            int numero1 = 3;
           int numero2 = 2;
             
           if(numero1 < numero2){
