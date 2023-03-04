63d889b0508cda5decd6c7a516e7d7a747b51a76
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-04 19:22:36 -0300

    Add lasagna

---
+++ src/edu/mrodrigochaves/lasanha/Lasagna.java
@@ -0,0 +1,26 @@
+package edu.mrodrigochaves.lasanha;
+
+public class Lasagna {
+
+    public static void main(String[] args) {
+        minhaLasanha();
+        
+
+    }
+
+    private void expectedMinutesInOven() {
+    }
+
+    public static String minhaLasanha() {
+        
+        Lasagna lasagna = new Lasagna();
+        lasagna.expectedMinutesInOven();
+        // => 40
+        return null;
+    }
+
+    
+
+    
+
+}
