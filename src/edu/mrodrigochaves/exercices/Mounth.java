e24d102f3c5a533dd354282226049310605c0632
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-04 18:42:44 -0300

    Add exercices

---
+++ src/edu/mrodrigochaves/exercices/Mounth.java
@@ -0,0 +1,57 @@
+package edu.mrodrigochaves.exercices;
+
+public class Mounth {
+
+    public static void main(String[] args) {
+
+        mesesDoAno();
+        minhasFerias();
+    }
+
+    private static void mesesDoAno() {
+        int mes = 0;
+        if (mes == 1) {
+            System.out.println("Janeiro");
+        } else if (mes == 2) {
+            System.out.println("Fevereiro");
+        } else if (mes == 3) {
+            System.out.println("Março");
+        } else if (mes == 4) {
+            System.out.println("Abril");
+        } else if (mes == 5) {
+            System.out.println("Maio");
+        } else if (mes == 6) {
+            System.out.println("Junho");
+        } else if (mes == 7) {
+            System.out.println("Julho");
+        } else if (mes == 8) {
+            System.out.println("Agosto");
+        } else if (mes == 9) {
+            System.out.println("Setembro");
+        } else if (mes == 10) {
+            System.out.println("Outubro");
+        } else if (mes == 11) {
+            System.out.println("Novembro");
+        } else if (mes == 12) {
+            System.out.println("Dezembro");
+        } else
+            System.out.println(
+                    "Não existe mês correspondente para este número.");
+    }
+
+    private static void minhasFerias() {
+
+        String mes = "dezembro";
+        switch (mes) {
+            case "dezembro":
+            case "julho":
+            case "janeiro":
+                System.out.println("Férias");
+                break;
+            default:
+                System.out.println("Mês Indefinido");
+                break;
+        }
+    }
+
+}
