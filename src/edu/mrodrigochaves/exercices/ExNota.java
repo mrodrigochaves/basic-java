f1e6efd0cf0e0da43e1901604c994cf399c31512
mrodrigochaves <dev.marciorodrigo@gmail.com>
2023-03-07 14:32:28 -0300

    Add nota

---
+++ src/edu/mrodrigochaves/exercices/ExNota.java
@@ -0,0 +1,22 @@
+package edu.mrodrigochaves.exercices;
+
+import java.util.Scanner;
+
+public class ExNota {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota:  ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println("Nota inválida! Digite novamente:  ");
+            nota = scan.nextInt();
+        }
+
+    }
+
+}
