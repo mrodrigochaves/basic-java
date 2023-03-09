c9534e1aabaf70e432dd281dc8cb7f7053288049
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-09 19:52:03 -0300

    Add Media

--- src/edu/mrodrigochaves/exercices/MaiorMedia.java
+++ src/edu/mrodrigochaves/exercices/MaiorMedia.java
@@ -1,5 +1,30 @@
 package edu.mrodrigochaves.exercices;
 
+import java.util.Scanner;
+
 public class MaiorMedia {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+        do {
+
+            System.out.println("Nú1mero é:  ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+
+            if (numero > maior) maior = numero;
+            
+            count = count + 1;
+
+        } while(count < 5);
+
+        System.out.println("O maior número é:  " + maior);
+        System.out.println("A media é:  " + (soma/5));
+    }
 }
