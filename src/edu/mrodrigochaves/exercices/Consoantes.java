689baf54764b4bf5087c94b485a71beb640d2f77
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-11 15:39:05 -0300

    Add consoantes

---
+++ src/edu/mrodrigochaves/exercices/Consoantes.java
@@ -0,0 +1,11 @@
+package edu.mrodrigochaves.exercices;
+
+import java.util.Scanner;
+
+public class Consoantes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
+    }
+}
