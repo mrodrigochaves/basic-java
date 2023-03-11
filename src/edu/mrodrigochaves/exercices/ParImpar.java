03d976aba2b6e7927b591bd2889266586ee329c4
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-11 14:16:22 -0300

    Add Par Impar

---
+++ src/edu/mrodrigochaves/exercices/ParImpar.java
@@ -0,0 +1,16 @@
+package edu.mrodrigochaves.exercices;
+
+import java.util.Scanner;
+
+public class ParImpar {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+        int quantNum;
+
+        System.out.println("Quantidade de números: ");
+        quantNum = scan.nextInt();        
+    }
+
+}
