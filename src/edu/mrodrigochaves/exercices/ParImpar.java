170da91529a244f477dfabde1fc0221d320eeac3
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-11 14:33:08 -0300

    finaly par impar

--- src/edu/mrodrigochaves/exercices/ParImpar.java
+++ src/edu/mrodrigochaves/exercices/ParImpar.java
@@ -8,9 +8,25 @@ public class ParImpar {
         Scanner scan = new Scanner(System.in);
     
         int quantNum;
+        int num;
+        int quantNumPares = 0, quantNumImpares = 0;
 
         System.out.println("Quantidade de números: ");
-        quantNum = scan.nextInt();        
+        quantNum = scan.nextInt();   
+        
+        int count = 0;
+        do{
+            System.out.println("Número: ");
+            num = scan.nextInt();
+
+            if (num % 2 == 0 ) quantNumPares++;
+            else quantNumImpares++;
+
+            count ++;
+        } while(count < quantNum);
+
+        System.out.println("Quantidade de número(s) Par(es): " + quantNumPares);
+        System.out.println("Quantidade de número(s) Impar(es): " + quantNumImpares);
     }
 
 }
